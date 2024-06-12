1cf3ee72609351ef031f432abe3d72a4fcf80971
Abhi <abhiram.bhat2005@gmail.com>
2024-06-12 21:27:44 +0100

    update

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# type: ignore
 from tokeniser import *
 from keras.models import Sequential
 from keras.layers import Dense
@@ -5,18 +6,17 @@ from keras.layers import Dropout
 from pandas import DataFrame
 from matplotlib import pyplot
 
-
 print("Running model.py")
 
 # Getting the data
 vocab = getter()    
 training_matrix, ytrain, train_docs, testing_matrix, ytest, test_docs = getter2(vocab)
 no_of_words = training_matrix.shape[1]
-
+global tokeniser
+tokeniser = Tokenizer()
 
 # Preperaring the data
 def prepare_data(train_docs, test_docs, mode) -> tuple:
-    tokeniser = Tokenizer()
     tokeniser.fit_on_texts(train_docs)
     training_matrix = tokeniser.texts_to_matrix(train_docs, mode=mode)
     testing_matrix = tokeniser.texts_to_matrix(test_docs, mode=mode)
@@ -27,7 +27,8 @@ def prepare_data(train_docs, test_docs, mode) -> tuple:
 def evaluate_model(training_matrix, ytrain, testing_matrix, ytest) -> list:
     scores = []
     no_of_words = training_matrix.shape[1]
-    for i in range(30):
+    n = 5                   # Number of times to run the model
+    for i in range(n):
         model = Sequential()
         model.add(Dense(50, input_shape=(no_of_words,), activation='relu'))
         model.add(Dense(1, activation='sigmoid'))
@@ -35,8 +36,8 @@ def evaluate_model(training_matrix, ytrain, testing_matrix, ytest) -> list:
         model.fit(training_matrix, ytrain, epochs=50, verbose=0)
         loss, acc = model.evaluate(testing_matrix, ytest, verbose=0)
         scores.append(acc)
-        print('%d accuracy: %s' % ((i+1), acc))
-        return scores
+        # print('%d accuracy: %s' % ((i+1), acc))
+    return model
 
 
 # Evaluates a sentiment analysis model using different tokenization modes
@@ -57,21 +58,23 @@ def model(train_docs, test_docs) -> None:
 '''
 
 # Predict the sentiment of a review
-def predict_sentiment(review, vocab, tokenizer, model) -> str:
+def predict_sentiment(review, vocab, tokeniser, model) -> str:
     tokens = clean_doc(review)
     tokens = [w for w in tokens if w in vocab]
     line = ' '.join(tokens)
-    encode = tokenizer.texts_to_matrix([line], mode='binary')
+    encode = tokeniser.texts_to_matrix([line], mode='binary')
     yhat = model.predict(encode, verbose=0)
     return round(yhat[0,0])
 
 
 # MAIN FUNCTION
 def main(train_docs, test_docs) -> None:
-    test_review = "love this movie"
-    tokenizer = Tokenizer()
-    print(predict_sentiment(test_review, vocab, tokenizer, model))
-
+    test_review_1 = "love this movie"
+    test_review_2 = "terrible"
+    training_matrix, testing_matrix = prepare_data(train_docs, test_docs, 'binary')
+    model = evaluate_model(training_matrix, ytrain, testing_matrix, ytest)
+    print("1 : ",predict_sentiment(test_review_1, vocab, tokeniser, model))
+    print("2 : ",predict_sentiment(test_review_2, vocab, tokeniser, model))
 
 
 if __name__ == '__main__':
